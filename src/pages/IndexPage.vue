644b3b0d666041da85d609ccb8720d2d8f57c0a7
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-02 16:33:46 +0200

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,72 +1,16 @@
 <!-- Template em Html -->
 <template>
   <q-page padding>
-    <button
-      style="position: absolute; right: 10px"
-      @click="counter++"
-    >
-      {{counter}}
-    </button>
-
-    <input type="text"
-      v-model="mensagem"
-      @keyup.esc="clearMesage"
-      @keyup.enter="alertMesage"
-    >
-    <button @click="clearMesage">
-      Limpar
-    </button>
-
-    <h5
-      v-if="mensagem.length"
-      class="border-grey">
-      {{ mensagem }}
-    </h5>
-
-
-    <h6 v-else>
-       No mesage enterd!😒
-    </h6>
-
-    <hr>
-
-    <p>Upercase: {{mesageUpercase}}</p>
-
   </q-page>
 </template>
 
 <!-- Script -->
 <script>
 import { defineComponent } from 'vue'
-
-export default defineComponent({
-  data(){
-    return{
-      mensagem: 'I love❤️ Vue.js',
-      counter: 0
-    }
-  },
-  computed:{
-
-    mesageUpercase(){
-      console.log('messageUpercase was edited!')
-      return this.mensagem.toUpperCase()
-    }
-
-  },
-  methods:{
-    clearMesage(){
-      this.mensagem = ''
-    },alertMesage(){
-      alert(this.mensagem)
-    }
-  }
-})
+export default defineComponent({})
 </script>
 
 <!-- Style -->
 <style>
-  .border-grey{
-    border: 1px solid grey;
-  }
+
 </style>
