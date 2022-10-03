4959ac61cb93c2e85c49ebf9911cd8c70a22adcc
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-03 23:26:33 +0200

    Start building the app

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,6 @@
 <!-- Template em Html -->
 <template>
-  <q-page padding>
-    <ul>
-
-      <task
-        v-for="(task, index) in tasks"
-        :task="task"
-        :index="index">
-      </task>
-    </ul>
-  </q-page>
+  <q-page padding></q-page>
 </template>
 
 <!-- Script -->
@@ -17,38 +8,7 @@
 import { defineComponent } from 'vue'
 import Task from '../components/Task.vue'
 
-export default defineComponent({
-
-    data(){
-      return{
-        tasks:[
-          {
-            name: 'Go to Shcoll',
-            dueDate:'2022/10/03',
-            dueTime: '09:00H'
-          },{
-            name: 'Go tO Hork',
-            dueDate:'2022/10/03',
-            dueTime: '10:00H'
-          },{
-            name: 'Go to GYM',
-            dueDate:'2022/10/03',
-            dueTime: '17:00H'
-          }
-        ]
-      }
-    }, methods:{
-      deleteTask(index){
-        this.tasks.splice(index, 1)
-
-      }
-    },
-    // importamos a pagina Task!
-    components:{
-    "task": require("components/Task.vue").default,
-    Task
-}
-})
+export default defineComponent({})
 </script>
 
 <!-- Style -->
