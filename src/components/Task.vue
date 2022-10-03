071529d6a1aa143dff5d9f7991ed70baaf075eb1
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-03 23:10:03 +0200

    Update Task.vue

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,14 +1,18 @@
 <!-- Template -->
 <tempalte>
   <li>
-    <div>{{index}} {{task.name}}</div>
-    <small>{{task.dueDate}} @{{task.dueTime}}</small>
+    <div> {{task.name}}  {{index}}</div>
+    <small> {{ task.dueDate }} @ {{ task.dueTime}} </small>
     <button class="bt-del" @click="deleteTask(index)">DEL</button>
   </li>
+  
 </tempalte>
 
 <!-- Script -->
 <script>
+  export default{
+    props:['task']
+  }
 
 </script>
 
