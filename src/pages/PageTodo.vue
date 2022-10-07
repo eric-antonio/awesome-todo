627c7d2715f9a15c33ee41b91f74ce9cc9571ffe
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-07 22:10:34 +0200

    Aplicando a chek box

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,7 +1,20 @@
 <!-- Template em Html -->
 <template>
   <q-page padding>
-      <p>Todo Page</p>
+    <q-list bordered >
+
+
+      <q-item v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="check1" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Notifications</q-item-label>
+
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -9,8 +22,29 @@
 <script>
 import { defineComponent } from 'vue'
 
-
-export default defineComponent({})
+export default defineComponent({
+  data(){
+    return{
+      tasks:[
+        {
+          id:1,
+          name: 'Go to Shop',
+          completed: false
+        },
+        {
+          id:2,
+          name: 'Go to Gym',
+          completed: false
+        },
+        {
+          id:3,
+          name: 'Go to Party',
+          completed: false
+        },
+      ]
+    }
+  }
+})
 </script>
 
 <!-- Style -->
