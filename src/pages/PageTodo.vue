56f5ac0e6743f4fd2094545d83c9256f925a6787
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-07 22:35:03 +0200

    Adicionando data e tempo!

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,12 +1,14 @@
 <!-- Template em Html -->
 <template>
-  <q-page padding>
+  <q-page class="q-pa-md">
     <q-list bordered >
 
 
       <q-item v-ripple
         v-for="task in tasks"
         :key="task.id"
+        @click="task.completed = !task.completed"
+        clickable
       >
         <q-item-section side top>
           <q-checkbox v-model="task.completed" />
@@ -14,6 +16,16 @@
 
         <q-item-section>
           <q-item-label>{{task.name}}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+
+          <q-item-label caption>{{task.dueDate}}</q-item-label>
+          <q-item-label caption>
+            <samll>
+              {{task.dueTime}}
+             </samll>
+          </q-item-label>
 
         </q-item-section>
       </q-item>
@@ -32,17 +44,26 @@ export default defineComponent({
         {
           id:1,
           name: 'Go to Shop',
-          completed: true
+          completed: true,
+          dueDate:'2022/09/01',
+          dueTime:'17:00h'
+
         },
         {
           id:2,
           name: 'Go to Gym',
-          completed: false
+          completed: false,
+          dueDate:'2022/09/06',
+          dueTime:'14:00h'
+
         },
         {
           id:3,
           name: 'Go to Party',
-          completed: false
+          completed: false,
+          dueDate:'2022/09/09',
+          dueTime:'12:00h'
+
         },
       ]
     }
